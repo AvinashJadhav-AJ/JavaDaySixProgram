3ca4656d4935d27f183440d054236a8c299d59cc
Avinash <aj912873@gmail.com>
2022-02-12 18:03:46 +0530

    Power OF Two

---
+++ src/com/BridgeLabz/java/PowerOfTwo.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.java;
+
+public class PowerOfTwo {
+	 public static int powerOfTwo(int num) {
+			if ((num & (num - 1)) == 0) {
+			    return 1;
+			} else {
+			    return 0;
+			}
+	 }
+}
